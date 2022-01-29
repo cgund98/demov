cbaf911e0b5e455d84ffe95ee075ee1acd5de1fa
Callum Gundlach <gundlachcallum@gmail.com>
2022-01-28 19:29:18 -0500

    Attempt to fix eslint dependencies

--- bin/service.ts
+++ bin/service.ts
@@ -3,6 +3,8 @@ import * as cdk from '@aws-cdk/core';
 import {DemovServiceStack} from '../lib/demov-service-stack';
 import {envSpecific} from '../src/util/environ';
 
+const four = 4;
+
 const app = new cdk.App();
 new DemovServiceStack(app, envSpecific('demov-service-stack'), {
   tags: {
